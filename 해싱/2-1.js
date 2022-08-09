58a5d2da51544d72c6bd5dfea147c87015a5baff
froggy1014 <clcl6084@gmail.com>
2022-08-09 15:02:32 +0900

    add: 2-1,2-2 problem solved

--- 해싱/2-1.js
+++ 해싱/2-1.js
@@ -1,28 +1,44 @@
-function solution(str){
-  let answer; 
-  let max = Number.MIN_SAFE_INTEGER
-  const map = new Map();
+// function solution(str){
+//   let answer; 
+//   let max = Number.MIN_SAFE_INTEGER
+//   const map = new Map();
 
-  for(let x of str){
-    if(map.has(x)){
-      let temp = map.get(x);
-      temp++;
-      map.set(x, temp);
-    }
-    else
-    {
-      let cnt = 0;
-      map.set(x, cnt);  
-    }
-  }
+//   for(let x of str){
+//     if(map.has(x)){
+//       map.set(x, map.get(x)+1);
+//     }
+//     else map.set(x, (map.get(x)||0)+1);  
+//   }
 
-  for(let k of str){
-    if(max < map.get(k)){
-      answer = k; 
-      max = map.get(k);
-    }  
-  }
+//   for(let k of str){
+//     if(max < map.get(k)){
+//       answer = k; 
+//       max = map.get(k);
+//     }  
+//   }
+//   return answer;
+// }
+// console.log(solution("AAAABBBCCDDDDD"))
 
-  return answer;
-}
-console.log(solution("AAAAABBCCDDDD"))
+// function solution(str){
+//   let answer; 
+//   let max = Number.MIN_SAFE_INTEGER
+//   const map = new Map();
+
+//   for(let x of str){
+//     if(map.has(x)){
+//       map.set(x, map.get(x)+1);
+//     }
+//     else map.set(x, (map.get(x)||0)+1);  
+//   }
+
+//   for(let [key,val] of map){
+//     if(max < val){
+//       max = val
+//       answer = key; 
+//     }  
+//   }
+//   return answer;
+// }
+// console.log(solution("AAAABBBCCDDDDD"))
+
--- 해싱/2-2.js
+++ 해싱/2-2.js
@@ -1,35 +1,40 @@
-function solution(str){
-  let answer;
-  let cnt = 1;
+// function solution(str){
+//   let answer;
+//   const map = new Map()
 
+//   for(let x of str)
+//   {
+//     if(map.has(x)) map.set(x, (map.get(x))+1)
+//     else map.set(x, 1)
+//   }
+
+//   console.log(map);
+
+//   for(let i = 0 ; i <= str.length ; i++){
+//     if(map.get(str[i]) === 1){
+//       answer = i+1;
+//       break;
+//     }
+//     else answer = -1;
+//   }
+//   return answer;
+// }
+
+// console.log(solution('statitsics'))
+
+function solution(str){
   const map = new Map()
 
-  for(let x of str)
-  {
-    if(map.has(x)){
-      let temp = map.get(x)
-      temp++
-      map.set(x, temp)
-    }
-    else{
-      map.set(x, 1)
-    }
-  }
-
+  for(let x of str) map.set(x, (map.get(x) ||0)+1)
+  
   console.log(map);
 
-  for(let t of str){
-    if(map.get(t) === 1){
-      answer = cnt;
-      break;
-    }
-    else{
-      answer = -1;
-    } 
-    cnt++
+  for(let i = 0 ; i <= str.length ; i++){
+    if(map.get(str[i]) === 1) return i+1;
   }
-  
-
-  return answer;
+  return -1;
 }
-console.log(solution('aabb'))
+console.log(solution('statitsics'))
+
+
+

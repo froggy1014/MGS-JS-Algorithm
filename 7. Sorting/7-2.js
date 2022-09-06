1b2c61f1274b9baac4d7af62e05e21fd8c615bc9
froggy1014 <clcl6084@gmail.com>
2022-09-06 21:01:11 +0900

    refactor: make 7-2 code better after online class

--- 7. Sorting/7-2.js
+++ 7. Sorting/7-2.js
@@ -1,14 +1,36 @@
+// 버블 정렬
+
+// function solution(nums){
+//   let answer = nums;
+//   let n = nums.length
+
+//   for(let i = n-1; i >= 0 ; i--){
+//     for(let j = 0; j < i; j++){
+//       if( nums[j] > nums[j+1]){
+//         [nums[j], nums[j+1]] = [nums[j+1], nums[j]]
+//       }
+//     }
+//   }
+//   return answer;
+// }
+// const nums = [5, 4, 2, 1, 3];
+// console.log(solution(nums))
+
 function solution(nums){
   let answer = nums;
+  let n = nums.length
 
-  for(let i = nums.length-1; i >= 0 ; i--){
-    for(let j = 0; j < i; j++){
+  for(let i = 0; i < n-1 ; i++){
+    for(let j = 0; j < n-i-1; j++){
       if( nums[j] > nums[j+1]){
         [nums[j], nums[j+1]] = [nums[j+1], nums[j]]
+      }
     }
   }
-}
   return answer;
 }
-const nums = [2, 8, 3, 6, 1, 7, 5, 9];
+const nums = [5, 4, 2, 1, 3];
 console.log(solution(nums))
+
+// nums.length - i - 1 로 이중포문 하나씩 차감함.
+

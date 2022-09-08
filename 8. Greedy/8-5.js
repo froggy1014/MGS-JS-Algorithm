c180e5b68ff5d6ffb48ebabdad563f25f60b698a
froggy1014 <clcl6084@gmail.com>
2022-09-08 23:29:02 +0900

    add: Greedy 8-5 done during class

---
+++ 8. Greedy/8-5.js
@@ -0,0 +1,13 @@
+function solution(nums){
+  let answer = 0;
+  nums.sort((a,b)=> a-b);
+
+  console.log(nums);
+
+  
+  
+
+  return answer; 
+}
+const nums = [2, 5, 3, 4, 2, 3];
+console.log(solution(nums))
